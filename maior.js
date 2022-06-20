390f6ac3aadabe61712b1cfa2a74f4005c95cf5b
Matheus Campos <matheus_501@hotmail.com>
2022-06-20 12:35:08 -0300

    modificacao no arquivo maior.js e map

--- maior.js
+++ maior.js
@@ -1,12 +1,17 @@
-var input = require('fs').readFileSync('stdin', 'utf8')
-var lines = input.split('\n')
+// var input = require('fs').readFileSync('stdin', 'utf8')
+// var lines = input.split('\n')
 
-var a = lines.shift()
-var b = a.split(' ')
+// var a = lines.shift()
+// var b = a.split(' ')
 
-for (let index = 0; index < b.length; index++) {
-  const element = b[index]
-  numeros.push(Number(element))
+// for (let index = 0; index < b.length; index++) {
+//   const element = b[index]
+//   numeros.push(Number(element))
+// }
+// var c = Math.max.apply(null, b)
+// console.log(`${c} eh o maior`)
+
+var nomes = ['matheus', 'jessica', 'laura', 'ana laura']
+for (let i = 0; i < nomes.length; i++) {
+  const nome = nomes[i]
 }
-var c = Math.max.apply(null, b)
-console.log(`${c} eh o maior`)
--- map.js
+++ map.js
@@ -1,29 +1,29 @@
 // -------------------------------MAP-----------------------------
 
 // MAP passa por todos os valores do array e executa uma função
-const numbers = [1, 3, 6, 8, 9, 17]
+const numbers = [1, 3, 6, 8, 9, 17];
 // console.log(numbers)
 // const doubledNumbers = numbers.map(function (elem) {
 //   return elem * 2
 // })
-const doubledNumbers = numbers.map(elem => elem * 2)
+const doubledNumbers = numbers.map((elem) => elem * 2);
 // console.log(doubledNumbers)
 
-const fahrenheit = [0, 32, 45, 50, 75, 80, 120]
+const fahrenheit = [0, 32, 45, 50, 75, 80, 120];
 // const celcius = fahrenheit.map(function (elem) {
 //   return Math.round(((elem - 32) * 5) / 9)
 // })
-const celcius = fahrenheit.map(elem => Math.round(((elem - 32) * 5) / 9))
+const celcius = fahrenheit.map((elem) => Math.round(((elem - 32) * 5) / 9));
 // console.log(celcius)
 
 // ------------------------FILTER-------------------------------//
 // quando eu preciso remover elementos indesejados usando algumas regras ( ou seja vou filtrar os elementos)
 
-const yetAnotherArray = [2, 3, 4, 5, 4, 12, 19, 7, 12, 19]
+const yetAnotherArray = [2, 3, 4, 5, 4, 12, 19, 7, 12, 19];
 const uniqueArray = yetAnotherArray.filter(
   (elem, index, arr) => arr.indexOf(elem) === index
-)
-console.log(uniqueArray)
+);
+console.log(uniqueArray);
 
 // ------------------------REDUCE--------------------------------
 const rockets = [
@@ -32,20 +32,20 @@ const rockets = [
   { country: 'China', launches: 16 },
   { country: 'Europe', launches: 7 },
   { country: 'India', launches: 5 },
-  { country: 'Japan', launches: 3 }
-]
+  { country: 'Japan', launches: 3 },
+];
 const totalLaunches = rockets.reduce(
   (preVal, elem) => preVal + elem.launches,
   0
-)
+);
 // console.log(totalLaunches)
 
 // -------------------------------EVERY-----------------------------
 
 // every verifica todos os elementos de um array retornando se isso é true or false (boolean)
 
-const anotherArray = [12, 5, 8, 120, 140]
-const allAbove10 = anotherArray.every(elem => elem > 10)
+const anotherArray = [12, 5, 8, 120, 140];
+const allAbove10 = anotherArray.every((elem) => elem > 10);
 // console.log(allAbove10)
 
 // neste problema identificar se todas as pessoas tem mais de 18 anos
@@ -53,10 +53,10 @@ const tchurma = [
   { id: 12, name: 'Marcos', age: 19 },
   { id: 47, name: 'Fred', age: 18 },
   { id: 77, name: 'Lara', age: 25 },
-  { id: 85, name: 'Luisa', age: 30 }
-]
+  { id: 85, name: 'Luisa', age: 30 },
+];
 
-const allAbove18 = tchurma.every(p => p.age >= 18)
+const allAbove18 = tchurma.every((p) => p.age >= 18);
 // console.log(allAbove18)
 
 // -------------------------------SOME-----------------------------
@@ -67,56 +67,56 @@ const allAbove18 = tchurma.every(p => p.age >= 18)
 function isPrime(value) {
   for (let i = 2; i < value; i++) {
     if (value % i === 0) {
-      return false
+      return false;
     }
   }
-  return value > 1
+  return value > 1;
 }
-const oneMoreArray = [6, 8, 11, 14, 42]
-const B = oneMoreArray.some(isPrime)
+const oneMoreArray = [6, 8, 11, 14, 42];
+const B = oneMoreArray.some(isPrime);
 // console.log(isPrime)
 
 const team = [
   { id: 12, name: 'Matheus', pilot: true },
   { id: 44, name: 'Yago', pilot: true },
   { id: 59, name: 'Lucas', pilot: false },
-  { id: 122, name: 'João', pilot: false }
-]
-const pilot = team.some(person => person.pilot)
+  { id: 122, name: 'João', pilot: false },
+];
+const pilot = team.some((person) => person.pilot);
 // console.log(pilot)
 
 // --------------------------------API REAL----------------------------
 
 function soma(a, b) {
-  var soma = a + b
-  return soma
+  var soma = a + b;
+  return soma;
 }
-var somaUm = soma(2, 5)
+var somaUm = soma(2, 5);
 // console.log(somaUm)
 
 function dizerNome(nome) {
   // console.log(' O nome é ' + nome)
 }
 
-dizerNome('Matheus')
+dizerNome('Matheus');
 
 function parimp(n) {
   if (n % 2 == 0) {
-    return 'par'
+    return 'par';
   } else {
-    return 'impar'
+    return 'impar';
   }
 }
-let res = parimp(12)
+let res = parimp(12);
 // console.log(res)
 
-const ages = [32, 33, 16, 40]
-const result = ages.filter(checkAdult => checkAdult >= 18)
-console.log(result)
+const ages = [32, 33, 16, 40];
+const result = ages.filter((checkAdult) => checkAdult >= 18);
+console.log(result);
 
 function checkAdult(age) {
-  return age >= 18
+  return age >= 18;
 }
 
-var nome = window.prompt('qual é o seu nome?')
-window.alert('É um grande prazer te conhecer' + nome)
+var nome = window.prompt('qual é o seu nome?');
+window.alert('É um grande prazer te conhecer' + nome);
